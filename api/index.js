d69750704e5be3d0d06d5e3a68efeb60e4dfd60d
Matheus Moraes <matheusmoraes85@hotmail.com>
2023-05-28 03:51:52 -0300

    feat

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes')
+const { sequelize } = require('./infra/models')
 
 const app = express()
 const port = 3000
@@ -10,3 +11,11 @@ app.use(express.json())
 router(app)
 
 app.listen(port || 3000, () => console.log(`Server is running on port ${port}`));
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco estabelecida com sucesso.');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar-se ao banco de dados:', error);
+  });
